b4d7ed9b0fee07eadf742370e57c6f0a4830492d
mrahman <mohammad.rahman@hbo.com>
2014-12-22 00:58:44 +0600

    +MetLife Module

---
+++ MetLife/src/test/java/SignIn.java
@@ -0,0 +1,14 @@
+import BaseApi.CommonApi;
+import org.testng.annotations.Test;
+
+/**
+ * Created by mrahman on 12/21/14.
+ */
+public class SignIn extends CommonApi{
+
+    @Test
+    public void tab(){
+        System.out.println(driver.getTitle());
+    }
+
+}
